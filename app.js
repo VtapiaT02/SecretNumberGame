86696b61b6eca641f2bd6fc5944513a88ce1f1fb
Victor Tapia <vtapia02@hotmail.com>
2024-02-07 11:08:17 -0600

    Funcionalidad

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+console.log(numeroSecreto)
+
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroSecreto === numeroUsuario){
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'intento' : 'intentos'}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+    }
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 asignarTextoElemento('h1', 'Juego del número secreto');
 asignarTextoElemento('p', 'Indica un número del 1 al 10');
-
-function intentoDeUsuario() {
-    
-}
